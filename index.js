f7545eabdf085a1b6d38196a811ca3c7efb61b63
yusufmhassan <you@example.com>
2024-01-20 04:12:17 +0100

    Your commit message here

---
+++ index.js
@@ -0,0 +1,27 @@
+/*const  express = require('express');
+const app = express();
+const port = 3000;
+no
+
+app.get('/',(req  , res) =>
+{res.send("hello it's me");});
+
+app.get('/user/:id', (req, res) => {
+    const userId = req.params.id;
+    res.send(`User ID: ${userId}`);
+  });
+  
+
+  /*const find = ( req, res, next) => {
+    console.log('hi who is that');
+
+  
+  next();}; 
+  app.use(find);*/
+
+
+  
+
+/*app.listen(port,(res,req) =>
+{console.log(`Server listening at http://localhost:${port}`);});*/
+Console.log('hello')
